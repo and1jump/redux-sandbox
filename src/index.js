b725bf7b3d5f3f9ae22e20082bc9b58d7857b915
Daniil <onejump9@gmail.com>
2019-02-14 13:24:15 +0300

    Reducer, redux store

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import { createStore } from "redux";
+
+const reducer = (state = 0, action) => {
+  switch (action.type) {
+    case "INC":
+      return state + 1;
+
+    default:
+      return state;
+  }
+};
+
+const store = createStore(reducer);
+store.subscribe(() => {
+  console.log(store.getState());
+});
+
+store.dispatch({ type: "INC" });
+store.dispatch({ type: "INC" });
